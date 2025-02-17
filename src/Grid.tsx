38f87468f1a89346dd32370d8ff22fbf51899587
ArjunMandakath <arjunms+1@gmail.com>
2025-02-16 22:04:10 -0500

    optimized grid

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import { LogicModule } from './GameLogic';
-
+///Global 
+const cellSize: number = 20 
+///Grid Type 
 interface GridProps {
   grid: number[][];
   onCellClick: (i: number, j: number) => void;
 }
-
+///Grid Component 
 const Grid: React.FC<GridProps> = ({ grid, onCellClick }) => {
   return (
-    <div
+    <div 
       style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${LogicModule.numCols}, 20px)`,
-        justifyContent: 'center',
-        gap: '1px',
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
       }}
-    >
-      {grid.map((row, i) =>
-        row.map((cell, j) => (
-          <div
-            key={`${i}-${j}`}
-            onClick={() => onCellClick(i, j)}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: LogicModule.getColor(cell),
-              border: '1px solid #999',
-            }}
-          />
-        )) 
-      )}
+    >  
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${LogicModule.numCols}, minmax(0, 1fr))`, 
+          gridTemplateRows: `repeat(${LogicModule.numRows}, minmax(0, 1fr))`,  
+          width: `${LogicModule.numCols * (cellSize + 1)}px`,  
+          height: `${LogicModule.numRows * (cellSize + 1)}px`, 
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        {grid.map((row, i) =>
+          row.map((cell, j) => (
+            <div
+              key={`${i}-${j}`}
+              onClick={() => onCellClick(i, j)}
+              style={{
+                width: cellSize,
+                height: cellSize,
+                border: '1px solid #999',
+                boxSizing: 'border-box',  
+                backgroundColor: LogicModule.getColor(cell),
+              }}
+            />
+          )) 
+        )}
+      </div>
     </div>
   );
 };
